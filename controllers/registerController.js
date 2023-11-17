504495314f8018a2e88720ee7d08d459cb91987b
marianagrana2 <mariana.grana2@gmail.com>
2023-11-16 20:33:37 -0600

    ✨ Token al user al hacer login listo

---
+++ controllers/registerController.js
@@ -0,0 +1,61 @@
+const {response, request} = require("express");
+const User = require("../models/usersModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// Registro Usuario con Hash en la contrasenia
+const registerUser = async (req = request, res = response) =>{
+    try{
+        const {userName, password, email } = req.body
+        const user = await User.findOne({userName})
+
+        if(!user) {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hashSync(password, salt)
+            const user = new User({userName, email, password: hashedPassword})
+            await user.save()
+            
+            res.status(201).json({
+                msg:"El usuario fue creado correctamente."
+            })
+        } else{
+            res.status(403).json({
+                msg:"El nombre de usuario ya existe."
+            })
+        }
+    }catch(error){
+        res.status(500).json({
+            msg:"Algo ocurrió al crear usuario.",
+            error
+        })
+    }
+}
+
+// Login usuario ya registrado anteriormente y al hacer login se crea su token.
+const loginUser = async (req = request, res = response) => {
+    const {userName, password } = req.body;
+
+    const user = await User.findOne({userName})
+    if(!user){
+        return res.status(403).json({
+            msg:"El usuario no fue encontrado."
+        })
+    }
+    const correctPassword = bcrypt.compareSync(password, user.password)
+
+    if(correctPassword){
+        const token = jwt.sign({id: user._id, userName: user.userName},process.env.JWT_SECRET)
+        res.status(200).json({
+            token
+        })
+    } else{
+        res.status(403).json({
+            msg:"La contraseña no es correcta."
+        })
+    }
+}
+
+module.exports = {
+    registerUser,
+    loginUser
+}
--- server.js
+++ server.js
@@ -8,9 +8,10 @@ class Server {
         this.app = express();
         this.port = process.env.PORT || 3003;
         this.database = new Database();
-        this.usersPath = "/api/v1/users"
-        this.productsPath = "/api/v1/products"
-        this.ordersPath = "/api/v1/orders"
+        this.usersPath = "/api/v1/users";
+        this.productsPath = "/api/v1/products";
+        this.ordersPath = "/api/v1/orders";
+        this.registerPath = "/api/v1/auth";
         this.dbConnection();
         this.middlewares();
         this.router();
@@ -29,6 +30,7 @@ class Server {
         this.app.use(this.usersPath,require("./routes/users.routes"), errors());
         this.app.use(this.productsPath,require("./routes/products.routes"));
         this.app.use(this.ordersPath, require("./routes/orders.routes"));
+        this.app.use(this.registerPath, require("./routes/register.routes"));
     }
 
     listen(){
